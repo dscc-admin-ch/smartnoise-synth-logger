5f1685d9ac24902e4781d62c1ac70a77614fc3d8
PaulineMauryL <pauline.maury-laribiere@bfs.admin.ch>
2024-08-12 10:11:10 +0200

    version 0.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name="smartnoise_synth_logger",
     packages=find_packages(),
-    version="0.0.2",
+    version="0.0.3",
     description="A logger wrapper for Smartnoise Synth Table Transformer",
     long_description=long_description,
     long_description_content_type="text/markdown",
